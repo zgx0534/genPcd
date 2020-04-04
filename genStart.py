2d1228af7e2a946c64162a6c606b7f2877cf0503
zgx0534 <563299377@qq.com>
2020-04-04 09:40:48 +0800

    init

---
+++ genStart.py
@@ -0,0 +1,25 @@
+import os
+import provider
+import numpy as np
+from pcdService import points2pcd
+
+# 存放路径
+PCD_DIR_PATH = os.path.join(os.path.abspath('.'), 'pcds')
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TRAIN_FILES = provider.getDataFiles(os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/train_files.txt'))
+train_file_idxs = np.arange(0, len(TRAIN_FILES))
+# 对每一个点云集合个数
+for fn in range(len(TRAIN_FILES)):
+    index = 0
+    # 获得一个点云集合
+    current_data, current_label = provider.loadDataFile(TRAIN_FILES[train_file_idxs[fn]])
+
+    # current_data = provider.jitter_point_cloud(current_data)
+    PCD_DIR_DETAIL_PATH = os.path.join(PCD_DIR_PATH, str(fn))
+    os.mkdir(PCD_DIR_DETAIL_PATH)
+    fileNum = current_data.shape[0]
+    for index in range(fileNum):
+        PCD_FILE_PATH = os.path.join(PCD_DIR_PATH, str(fn) + '_' + str(index) + '.pcd')
+        points2pcd(current_data[index], PCD_FILE_PATH)
+        index = index + 1
---
+++ pcdService.py
@@ -0,0 +1,30 @@
+import os
+
+
+# 传入点云对象
+def points2pcd(points, PCD_FILE_PATH):
+
+    if os.path.exists(PCD_FILE_PATH):
+        os.remove(PCD_FILE_PATH)
+
+    # 写文件句柄
+    handle = open(PCD_FILE_PATH, 'a')
+
+    # 得到点云点数
+    point_num = points.shape[0]
+
+    # pcd头部（重要）
+    handle.write(
+        '# .PCD v0.7 - Point Cloud Data file format\nVERSION 0.7\nFIELDS x y z\nSIZE 4 4 4\nTYPE F F F\nCOUNT 1 1 1')
+    string = '\nWIDTH ' + str(point_num)
+    handle.write(string)
+    handle.write('\nHEIGHT 1\nVIEWPOINT 0 0 0 1 0 0 0')
+    string = '\nPOINTS ' + str(point_num)
+    handle.write(string)
+    handle.write('\nDATA ascii')
+
+    # 依次写入点
+    for i in range(point_num):
+        string = '\n' + str(points[i, 0]) + ' ' + str(points[i, 1]) + ' ' + str(points[i, 2])
+        handle.write(string)
+    handle.close()
